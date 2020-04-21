543a79bcd3531866b4b05fcb82705d5364d779e6
Adam Lavin <adam.lavin@upp.ai>
2020-04-21 22:07:06 +0100

    Add missing items ts file

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,7 @@
 yarn ts-node index.ts
 
 yarn quicktype out/creatures.json -o out/creatures.ts --just-types
+yarn quicktype out/items.json -o out/items.ts --just-types
 yarn quicktype out/nookMiles.json -o out/nookMiles.ts --just-types
 yarn quicktype out/recipes.json -o out/recipes.ts --just-types
 yarn quicktype out/villagers.json -o out/villagers.ts --just-types
