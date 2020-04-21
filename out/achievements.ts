e75a7c5c68f83bcd4eb046e4171e100ed52df4ac
Adam Lavin <adam.lavin@upp.ai>
2020-04-21 21:57:53 +0100

    Add missing files

---
+++ out/achievements.ts
@@ -0,0 +1,27 @@
+export interface Achievements {
+  sourceSheet: SourceSheet;
+  name: string;
+  awardCriteria: string;
+  num: number;
+  internalId: number;
+  internalName: string;
+  internalCategory: string;
+  ofTiers: number;
+  tier1: number;
+  tier2: number;
+  tier3: number;
+  tier4: number;
+  tier5: number;
+  rewardTier1: number;
+  rewardTier2: number;
+  rewardTier3: number;
+  rewardTier4: number;
+  rewardTier5: number;
+  rewardTier6: number;
+  sequential: boolean;
+  uniqueEntryId: string;
+}
+
+export enum SourceSheet {
+  Achievements = 'Achievements',
+}
