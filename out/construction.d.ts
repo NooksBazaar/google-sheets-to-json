ecfff0557fb036dadda60ced3fd890cbeebd4c79
Adam Lavin <adam.lavin@upp.ai>
2020-04-22 22:48:42 +0100

    Apparently that rename didn't work properly, here's the actual files

---
+++ out/construction.d.ts
@@ -0,0 +1,30 @@
+export interface Construction {
+  sourceSheet: SourceSheet;
+  name: string;
+  image: string;
+  buy: number;
+  category: Category;
+  source: Source[];
+  filename: string;
+  uniqueEntryId: string;
+}
+
+export enum Category {
+  Bridge = 'Bridge',
+  Door = 'Door',
+  Incline = 'Incline',
+  Mailbox = 'Mailbox',
+  Roofing = 'Roofing',
+  Siding = 'Siding',
+}
+
+export enum Source {
+  ResidentServicesUpgrade = 'Resident Services Upgrade',
+  The3RDHouseUpgradeLeftRoom = '3rd House Upgrade (Left Room)',
+  The4ThHouseUpgradeRightRoom = '4th House Upgrade (Right Room)',
+  The5ThHouseUpgrade2NdFloor = '5th House Upgrade (2nd Floor)',
+}
+
+export enum SourceSheet {
+  Construction = 'Construction',
+}
