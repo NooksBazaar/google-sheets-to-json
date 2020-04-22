23d00b4747d61ff250723fa8de88c629bbcd71fe
Adam Lavin <adam.lavin@upp.ai>
2020-04-22 22:46:13 +0100

    Update build and initial prepare to publish to npm

--- bin/build.sh
+++ bin/build.sh
@@ -2,13 +2,15 @@
 
 yarn ts-node index.ts
 
-yarn quicktype out/creatures.json -o out/creatures.ts --just-types
-yarn quicktype out/items.json -o out/items.ts --just-types
-yarn quicktype out/nookMiles.json -o out/nookMiles.ts --just-types
-yarn quicktype out/recipes.json -o out/recipes.ts --just-types
-yarn quicktype out/villagers.json -o out/villagers.ts --just-types
-yarn quicktype out/construction.json -o out/construction.ts --just-types
-yarn quicktype out/achievements.json -o out/achievements.ts --just-types
+yarn quicktype out/creatures.json -o out/creatures.d.ts --just-types
+yarn quicktype out/items.json -o out/items.d.ts --just-types
+yarn quicktype out/nookMiles.json -o out/nookMiles.d.ts --just-types
+yarn quicktype out/recipes.json -o out/recipes.d.ts --just-types
+yarn quicktype out/villagers.json -o out/villagers.d.ts --just-types
+yarn quicktype out/construction.json -o out/construction.d.ts --just-types
+yarn quicktype out/achievements.json -o out/achievements.d.ts --just-types
 
 yarn prettier:fix
 yarn prettier "out/*.json" --write
+
+cp package.json out
