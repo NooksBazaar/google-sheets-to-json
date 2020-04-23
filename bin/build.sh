6e426ac8ef7e5dc6728aecc28d2b2ad2791eb378
Adam Lavin <adam.lavin@upp.ai>
2020-04-23 22:41:45 +0100

    Enums are still usable values I guess, convert .d.ts files to .ts

--- bin/build.sh
+++ bin/build.sh
@@ -2,13 +2,13 @@
 
 yarn ts-node index.ts
 
-yarn quicktype out/creatures.json -o out/creatures.d.ts --just-types -t Creatures
-yarn quicktype out/items.json -o out/items.d.ts --just-types -t Items
-yarn quicktype out/nookMiles.json -o out/nookMiles.d.ts --just-types -t NookMiles
-yarn quicktype out/recipes.json -o out/recipes.d.ts --just-types -t Recipes
-yarn quicktype out/villagers.json -o out/villagers.d.ts --just-types -t Villagers
-yarn quicktype out/construction.json -o out/construction.d.ts --just-types -t Construction
-yarn quicktype out/achievements.json -o out/achievements.d.ts --just-types -t Achievements
+yarn quicktype out/creatures.json -o out/creatures.ts --just-types -t Creatures
+yarn quicktype out/items.json -o out/items.ts --just-types -t Items
+yarn quicktype out/nookMiles.json -o out/nookMiles.ts --just-types -t NookMiles
+yarn quicktype out/recipes.json -o out/recipes.ts --just-types -t Recipes
+yarn quicktype out/villagers.json -o out/villagers.ts --just-types -t Villagers
+yarn quicktype out/construction.json -o out/construction.ts --just-types -t Construction
+yarn quicktype out/achievements.json -o out/achievements.ts --just-types -t Achievements
 
 yarn prettier:fix
 yarn prettier "out/*.json" --write
